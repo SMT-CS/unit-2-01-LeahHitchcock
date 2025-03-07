989d1730c9bc73a4478a6d09a6c2a7927b60bec9
LeahHitchcock <leah.hitchcock@stu.ocsb.ca>
2025-03-07 18:41:55 +0000

    added the code

---
+++ main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+""""
+Created by: Leah H
+Created on: Febuary 24 2025
+This function calculates circumference 
+"""
+
+import math
+
+
+def main() -> None:
+    """
+    The main() function calculates the area of a 42 mm circle,
+        returns None.
+    """
+    print("If a circle has a radius of 42 mm:")
+    print("")
+    print(f"Area is {(math.pi * 42):.5f} mm².")
+
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
